c298ff2843308e38508d93f6f26c92ad9afc761c
Zander Work <zander@zanderwork.com>
2019-02-05 19:36:58 -0800

    did more payload tests (#6)

--- test/test_payload.py
+++ test/test_payload.py
@@ -2,14 +2,38 @@ import unittest
 from modules.payloads import bash_payload, teensy_payload
 
 class TestPayload(unittest.TestCase):
+    def setUp(self):
+        # setup bash and teensy payloads for further tests
+        self.bp = bash_payload.payload()
+        self.tp = teensy_payload.payload()
+
+        # disable pwntools log
+        pwn.context.log_level = 'critical'
+        
+        # start proc
+        self.eggshell_proc = pwn.process(["python", "eggshell.py"])
+
+        # make payload
+        self.eggshell_proc.sendline("3")
+
+    def tearDown(self):
+        self.eggshell_proc.kill()
+
     def test_bash_instance(self):
-        bp = bash_payload.payload()
         print "== Instantiated bash payload =="
-        self.assertTrue(isinstance(bp, bash_payload.payload))
+        self.assertTrue(isinstance(self.bp, bash_payload.payload))
         print "== Is a bash payload, confirmed =="
 
     def test_teensy_instance(self):
         tp = teensy_payload.payload()
         print "== Instantiated teensy payload =="
-        self.assertTrue(isinstance(tp, teensy_payload.payload))
+        self.assertTrue(isinstance(self.tp, teensy_payload.payload))
         print "== Is a teensy payload, confirmed =="
+
+    def test_bash_properties(self):
+        print "== Testing bash properties =="
+        self.assertTrue(self.bp.name == "bash")
+
+    def test_teensy_properties(self):
+        print "== Testing teensy properties =="
+        self.assertTrue(self.tp.name == "Teensy macOS")
