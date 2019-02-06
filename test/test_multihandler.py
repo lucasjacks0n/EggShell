8b64e8d8a3c2a320dc0d7fa3ef75bf6528bad83b
okonekp <okonekp@oregonstate.edu>
2019-02-05 18:23:18 -0800

    Added Multihandler tests and added some pretty text

---
+++ test/test_multihandler.py
@@ -0,0 +1,31 @@
+import unittest
+from modules import server
+from modules import multihandler
+
+class TestMultiHandler(unittest.TestCase):
+   @classmethod
+   def setUpClass(self):
+      print "============ Testing Multihandler ============"
+   def test_multihandler_instance(self):
+      server_obj = server.Server()
+      multihandler_obj = multihandler.MultiHandler(server_obj)
+      print "== Testing Multihandler Initialization =="
+      self.assertTrue(isinstance(multihandler_obj,multihandler.MultiHandler))
+      print "== Created a Multihandler =="
+   def test_close_no_server(self):
+      print "== Trying to close all server with no sessions =="
+      server_obj = server.Server()
+      multihandler_obj = multihandler.MultiHandler(server_obj)
+      multihandler_obj.stop_server()
+      self.assertTrue(1)
+      print "== We didn't raise, success =="
+   def test_background_server(self):
+      print "== Testing Background Server Creation =="
+      server_obj = server.Server()
+      server_obj.port = 4444
+      multihandler_obj = multihandler.MultiHandler(server_obj)
+      multihandler_obj.start_background_server()
+      print "== Created Background Server =="
+      multihandler_obj.stop_server()
+      print "== Started and Closed Successfully =="
+      
