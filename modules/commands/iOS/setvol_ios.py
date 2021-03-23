6b059e2b777c21205f1db406c9200b7a54c7971b
Luca <orksec@protonmail.com>
2021-03-23 15:03:14 +0100

    Changed argument

--- modules/commands/iOS/setvol_ios.py
+++ modules/commands/iOS/setvol_ios.py
@@ -2,7 +2,7 @@ class command:
     def __init__(self):
         self.name = "setvol"
         self.description = "set device volume"
-        self.usage = "Usage: volume 1.0"
+        self.usage = "Usage: volume <number>"
 
     def run(self, session, cmd_data):
         if not cmd_data['args']:
